7a11df5d8c749318f3d08cf2459904a6f07882b7
Jidhiya Vijayan <jidhiya@outlook.com>
2023-12-14 13:40:52 -0500

    Initial Commit

---
+++ Coding_In_Java/src/ArraysInterviewQuestions/ArrayMinMax.java
@@ -0,0 +1,15 @@
+package ArraysInterviewQuestions;
+
+import java.util.Arrays;
+
+public class ArrayMinMax {
+
+	public static void main(String[] args) {
+	
+		int a[] = {1,423,6,46,34,23,13,53,4}; 
+		Arrays.sort(a);
+		System.out.println("Min : "+ a[0] + "\nMax : " + a[a.length-1]);
+		
+	}
+
+}
